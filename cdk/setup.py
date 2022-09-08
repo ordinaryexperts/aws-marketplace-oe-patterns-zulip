e6794de2d7e7dad350c224f0d031a6f24c1c91d5
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-09-08 12:24:02 -0700

    bump version again

--- cdk/setup.py
+++ cdk/setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     install_requires=[
         f"aws-cdk-lib=={CDK_VERSION}",
         f"constructs>=10.0.0,<11.0.0",
-        f"oe-patterns-cdk-common@git+https://github.com/ordinaryexperts/aws-marketplace-oe-patterns-cdk-common@079723efcc9b82a41e271cfe8069f133c209006e"
+        f"oe-patterns-cdk-common@git+https://github.com/ordinaryexperts/aws-marketplace-oe-patterns-cdk-common@f4ad89b1b1838aa000716d185437ed253d907a33"
     ],
 
     python_requires=">=3.6",
