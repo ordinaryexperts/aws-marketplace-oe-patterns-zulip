512f79154420a1790803f4ce6a7b81ec1fbe6aaa
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-10-13 12:18:07 -0700

    upgrading common

--- cdk/setup.py
+++ cdk/setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     install_requires=[
         f"aws-cdk-lib=={CDK_VERSION}",
         f"constructs>=10.0.0,<11.0.0",
-        f"oe-patterns-cdk-common@git+https://github.com/ordinaryexperts/aws-marketplace-oe-patterns-cdk-common@1d841b710ceb98cd0ecfad8c60e7872c15b108ca"
+        f"oe-patterns-cdk-common@git+https://github.com/ordinaryexperts/aws-marketplace-oe-patterns-cdk-common@3.2.0"
     ],
 
     python_requires=">=3.6",
